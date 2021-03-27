1ab44bdd00e1c80f8e3b671bf513be9078593a93
yasser-khelalef <yasser.khelalef@g.enp.edu.dz>
2021-03-27 02:48:38 +0100

    test 2 and 3 added

--- tests 2 & 3 & 4/functional.py
+++ tests 2 & 3 & 4/functional.py
@@ -35,5 +35,40 @@ def sum_of_int_str_items(list_of_strings):
     return print("The sum of integer items in the given list is : ",sum_of_items)
 
 
+def persistence(number):
+    # This is a recursive function
+    if type(number)==int:
+        # The base case when the number contains only one digits
+        if len(str(number))==1:
+            return 0
 
-    
+
+        # The recursive case
+        else:
+            # Convert the number into a string to be able to iterate through digits
+            num_into_str = str(number)
+
+            # Initializing the product of digits to 1
+            product = 1
+            
+            for num in num_into_str:
+                '''
+                Iterating through digits (because now the number is considered as a string)
+                Multiplying all the digits together after first converting each one of them into an integer
+                '''
+                product = product*int(num)
+            '''
+            returnin 1  because the multiplication occured and calling again the function.
+            if the product now contains one digit only, the function will return 1, otherwise it will perfom 
+            the multiplication again and add another 1 to the old 1 we had previously.
+            the function keeps doing so until we have only one digit in the number, and we get the number 
+            of how many times the multiplication occured
+            '''
+            return 1 + persistence(product)
+    else:
+        return "Please enter an integer"
+            
+
+        
+
+        
